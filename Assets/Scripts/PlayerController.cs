0ade5d66113875c98c10d51e7e53933c22053c0e
Despair <akmashev2002@gmail.com>
2020-11-10 23:27:41 +0300

    First commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using System.Threading;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float sensivity;         //сенса
+    private Vector3 mousePos;
+    private float MyAngle = 0F;
+
+    public float speed;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
+    }
+
+    void Update()
+    {
+        mousePos = Input.mousePosition;
+    }
+
+    void FixedUpdate()
+    {
+        MouseRotate();
+        MovementLogic();
+    }
+
+    private void MovementLogic()            //Движение
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        transform.Translate(movement * speed);
+    }
+
+    private void MouseRotate()          //Крутить башкой
+    {
+        MyAngle = 0;
+        MyAngle = sensivity * ((mousePos.x - (Screen.width / 2)) / Screen.width);
+
+        transform.RotateAround(transform.position, transform.up, MyAngle);
+    }
+}
---
+++ Assets/Scripts/Trigger.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trigger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Renderer render = GetComponent<Renderer>();
+        render.material.color = Color.green;
+    }
+}
